1e4fed148cb79bebc2ae1ad760cbae655dd8f7e8
LuisTaborda <luisgustavo.taborda@gmail.com>
2021-01-05 19:59:40 -0300

    adicionando modal para confirmar deleção de lançamentos

--- src/views/lancamentos/consulta-lancamentos.js
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -11,6 +11,9 @@ import LocalStorageService from '../../app/services/localStorageService'
 
 import * as messages from '../../components/toastr'
 
+import {Dialog} from 'primereact/dialog'
+import {Button} from 'primereact/button'
+
 class ConsultaLancamentos extends React.Component{
 
     state = {
@@ -18,6 +21,8 @@ class ConsultaLancamentos extends React.Component{
         mes : '',
         tipo: '',
         descricao: '',
+        showConfirmDialog : false,
+        lancamentoDeletar : [],
         lancamentos : []
     }
 
@@ -30,18 +35,28 @@ class ConsultaLancamentos extends React.Component{
         console.log('editando lançamento ',id)
     }
 
-    deletar = ( lancamento ) => {
-        this.service.deletar(lancamento.id).then(response => {
+    abrirConfirmacao = (lancamento) => {
+        this.setState({showConfirmDialog : true, lancamentoDeletar : lancamento})
+    }
+
+    cancelarDelecao = () => {
+        this.setState({showConfirmDialog : false, lancamentoDeletar : {} })
+
+    }
+
+    deletar = () => {
+        this.service.deletar(this.state.lancamentoDeletar.id).then(response => {
             const lancamentos = this.state.lancamentos
-            const index = lancamentos.indexOf(lancamento)
+            const index = lancamentos.indexOf(this.state.lancamentoDeletar)
             lancamentos.splice(index, 1)
-            this.setState(lancamentos)
-            
+            this.setState({lancamentos : lancamentos, showConfirmDialog : false})
+
             messages.mensagemSucesso('Lançamento deletado com sucesso!')
         }).catch(eror => {
             messages.mensagemErro('Ocorreu um erro ao tentar deletar o lançamento, tente novamente mais tarde.')
         })
     }
+
     buscar = () =>{
         if(!this.state.ano){
             messages.mensagemErro('O preenchimento do campo ano é obrigatório!');
@@ -68,8 +83,15 @@ class ConsultaLancamentos extends React.Component{
 
     render(){
         const meses = this.service.obterListaMeses()
-
         const tipos = this.service.obterListaTipos();
+
+        const confirmDialogFooter = (
+                <div>
+                    <Button label="Confirmar" icon="pi pi-check" onClick={this.deletar} autoFocus />
+                    <Button label="Cancelar" icon="pi pi-times" onClick={this.cancelarDelecao} className="p-button-secondary" />
+                </div>
+        )
+    
 
         return(
             <Card title="Consula Lançamento">
@@ -108,9 +130,20 @@ class ConsultaLancamentos extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <div className="bs-component">
-                            <LancamentosTable lancamentos={this.state.lancamentos} deleteAction={this.deletar} editAction={this.editar}/>
+                            <LancamentosTable lancamentos={this.state.lancamentos} deleteAction={this.abrirConfirmacao} editAction={this.editar}/>
                         </div>
                     </div>
+                </div>
+
+                <div>
+                    <Dialog header="Confirmação" 
+                            visible={this.state.showConfirmDialog} 
+                            style={{ width: '50vw' }} 
+                            modal={true} 
+                            onHide={() => this.setState({showConfirmDialog : false})}
+                            footer={confirmDialogFooter}>
+                        <p>Tem certeza que deseja excluir este lançamento?</p>
+                    </Dialog>
                 </div>
             </Card>
         )
