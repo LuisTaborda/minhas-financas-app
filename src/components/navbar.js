43c7a257604d2e51653ca79ca8b7407b8bc9d6f9
LuisTaborda <luisgustavo.taborda@gmail.com>
2021-01-06 20:46:26 -0300

    implementação de autentificação para rotas e usuario deslogar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import NavbarItem from './navbarItem'
 
+import AuthService from '../app/services/authService'
+
+const deslogar = () => {
+    AuthService.removerUsuarioAutenticado()
+}
+
 function Navbar(){
     return(
         <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
@@ -14,7 +20,7 @@ function Navbar(){
                     <NavbarItem href="#/home" label="Home"/>
                     <NavbarItem href="#/cadastro-usuario" label="Usuários"/>
                     <NavbarItem href="#/consulta-lancamentos" label="Lançamentos"/>
-                    <NavbarItem href="#/login" label="Login"/>
+                    <NavbarItem onClick={deslogar} href="#/login" label="Sair"/>
                 </ul>
 
                 </div>
--- src/main/rotas.js
+++ src/main/rotas.js
@@ -6,17 +6,36 @@ import CadastroUsuario from '../views/cadastroUsuario'
 import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos'
 import CadastroLancamentos from '../views/lancamentos/cadastro-lançamentos'
 
-import { Route, Switch, HashRouter } from 'react-router-dom' 
+import { Route, Switch, HashRouter, Redirect} from 'react-router-dom' 
+import AuthService from '../app/services/authService'
+
+
+
+function RotaAutenticada({ component: Component, ...props}){
+    return (
+        <Route {...props} render={ (componentProps) => {
+           if(AuthService.isUsuarioAutenticado()){
+               return (
+                    <Component {...componentProps}/>
+               )
+           } else {
+               return(
+                    <Redirect to ={ {pathname : '/login', state : { from : componentProps.location } } }/>
+               ) 
+           }
+        }} />
+    )
+}
 
 function Rotas(){
     return(
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home}></Route>
-                <Route path="/login" component={Login}></Route>
-                <Route path="/cadastro-usuario" component={CadastroUsuario}></Route>
-                <Route path="/consulta-lancamentos" component={ConsultaLancamentos}></Route>
-                <Route path="/cadastro-lancamentos/:id?" component={CadastroLancamentos}></Route>
+                <Route path="/login" component={Login}/>
+                <Route path="/cadastro-usuario" component={CadastroUsuario}/>
+                <RotaAutenticada path="/home" component={Home}/>
+                <RotaAutenticada path="/consulta-lancamentos" component={ConsultaLancamentos}/>
+                <RotaAutenticada path="/cadastro-lancamentos/:id?" component={CadastroLancamentos}/>
             </Switch>
         </HashRouter>
     )
